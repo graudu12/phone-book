c3ca894428327ff2d8aca58fef69daa1adaac028
Dima <graudu_12@yahoo.co.uk>
2025-06-26 18:44:03 +0100

    creat_repository

--- src/redux/auth/selectors.js
+++
@@ -1,9 +0,0 @@
-export const selectLoggedIn = (state) => state.auth.isLoggedIn;
-
-export const selectUser = (state) => state.auth.user;
-
-export const selectRefreshing = (state) => state.auth.isRefreshing;
-
-export const selectLoading = (state) => state.auth.loading;
-
-export const selectError = (state) => state.auth.error;
